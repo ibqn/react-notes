fc2226f86568d53778044312422d58c80465d925
ibqn <evgen.ibqn@gmail.com>
2021-06-12 00:33:49 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Getting Started with Create React App
+
+App was created with
+
+```shell
+yarn create react-app react-notes
+```
---
+++ src/components/global-styles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+const styled = { createGlobalStyle }
+const GlobalStyles = styled.createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
+
+export default GlobalStyles
