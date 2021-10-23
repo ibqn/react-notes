e69b8fbd67d11ed23ecba15d6c57309fe84dd6c1
ibqn <evgen.ibqn@gmail.com>
2021-10-23 16:44:59 +0200

    use border box

--- src/components/global-styles.js
+++ src/components/global-styles.js
@@ -3,6 +3,10 @@ import { createGlobalStyle } from 'styled-components'
 const styled = { createGlobalStyle }
 
 const GlobalStyles = styled.createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
